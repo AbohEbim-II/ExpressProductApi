d54645995063a328994378be6d0f3bc3c7960ae5
Aboh James Ebim II <Abohjamesebim@gmail.com>
2025-12-28 23:36:26 +0100

    Get a single product by Id done

--- products.js
+++ products.js
@@ -8,28 +8,19 @@ let products = [
 ]
 
 router.get('/', (req, res) => {
-    res.json(products)
+    res.status(200).json(products)
 })
 
-router.post('/', (req, res) => {
-  const { id, name, price } = req.body;
-  if (!id || !name || price === undefined) {
-    return res.status(400).json({
-      error: "id, name, and price are required"
-    });
-  }
+router.get('/:id', (req, res) => {
+  const productId = parseInt(req.params.id);
+  
+  const product = products.find(p => p.id === productId);
 
-  if (typeof price !== 'number' || price < 0) {
-    return res.status(400).json({
-      error: "price must be a non-negative number"
-    });
-  }
-  let newProduct = { id, name, price };
-  products.push(newProduct);
-  res.status(201).json(newProduct);
-});
-
-
+  if (!product) {
+    return res.status(404).json({ error: "Product not found" });
+}
+  res.status(200).json(product)
+})
 
 
 export default router
